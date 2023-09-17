19a17bc5c8d9d3e1822e3aa2fa9980ead25adac6
Marcus Dahl <marcus.dahl@everyday.no>
2023-09-17 02:01:00 +0200

    fix: parse_function spans

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -535,6 +535,8 @@ impl Parser {
 
         let body = self.parse_block()?;
 
+        self.consume_node()?;
+
         self.expect(Keyword::End)?;
 
         Ok(self.produce_node(Function::new(params, body)))
