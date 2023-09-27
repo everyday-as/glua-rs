88c749dc4c710bf91f461cce938eeb98dfed9b97
J0sh0nat0r <jojo.parillon@gmail.com>
2023-09-27 22:41:45 +0100

    refactor: remove benches

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,3 @@
-#![feature(test)]
-extern crate test;
-
 pub use self::parser::Parser;
 
 pub mod ast;
@@ -10,8 +7,7 @@ pub mod parser;
 #[cfg(test)]
 mod tests {
     use std::fs::File;
-    use std::io::{Read, Write};
-    use test::bench::Bencher;
+    use std::io::Write;
 
     use crate::lexer::lex;
     use crate::Parser;
@@ -31,21 +27,5 @@ mod tests {
         // dbg!(&chunk);
 
         write!(File::create("test.parsed").unwrap(), "{:#?}", chunk).unwrap();
-    }
-
-    #[bench]
-    fn lexer(b: &mut Bencher) {
-        b.iter(|| lex(CODE).unwrap());
-    }
-
-    #[bench]
-    fn parser(b: &mut Bencher) {
-        b.iter(|| {
-            let tokens = lex(CODE).unwrap();
-
-            let mut parser = Parser::new(tokens);
-
-            parser.parse_chunk().unwrap()
-        });
     }
 }
